aea86120a872ae2955eaba7ea457cec6d0773f82
lidner <lidner.ls@gmail.com>
2022-04-05 19:05:18 +0600

    Webpack analyzer, app finished

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+	.BundleAnalyzerPlugin;
 
 module.exports = {
 	mode: 'development',
@@ -8,13 +10,40 @@ module.exports = {
 	},
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: '[name].js',
+		filename: '[name][contenthash].js',
+		clean: true,
+		assetModuleFilename: '[name][ext]',
+	},
+	devtool: 'source-map',
+	devServer: {
+		static: {
+			directory: path.resolve(__dirname, 'dist'),
+		},
+		port: 3000,
+		open: true,
+		hot: true,
+		compress: true,
+		historyApiFallback: true,
 	},
 	module: {
 		rules: [
 			{
 				test: /\.scss$/,
 				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+					},
+				},
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
 			},
 		],
 	},
@@ -24,5 +53,6 @@ module.exports = {
 			filename: 'index.html',
 			template: 'src/template.html',
 		}),
+		new BundleAnalyzerPlugin(),
 	],
 };
